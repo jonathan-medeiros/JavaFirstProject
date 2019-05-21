3fda2552a70dd7f27cf8de45a838cbbefa546ae3
jonathan-medeiros <jonathan.d.medeiros@gmail.com>
2019-05-21 18:42:21 -0300

    Including for

--- src/JavaFirstProject.java
+++ src/JavaFirstProject.java
@@ -3,9 +3,10 @@ public class JavaFirstProject {
     public static void main(String[] args) {
         System.out.println("Ready!!");
 
-        System.out.println("Ready2!!");
+        for (int i = 1; i<=10 ; i++){
+            System.out.println("Number " + i);
+        }
 
-        System.out.println("Ready3!!");
 
     }
 }
